8e135f4aa999216fcec9ccb62dedeb1883e34618
r-svp26 <riteshk6341@gmail.com>
2021-09-10 16:17:45 +0530

    [Ritesh] Add: Check employee attendence

--- Employee.js
+++ Employee.js
@@ -1,1 +1,9 @@
-console.log("Welcome to Employee Wage Program");
+/* UC1-employee attendence */
+const IS_PRESENT = 1;
+let empCheck = Math.floor(Math.random()* 10) % 2;
+if(empCheck == IS_PRESENT){
+    console.log("Employee is present");
+    return;
+} else{
+    console.log("Employee is absent");
+}
