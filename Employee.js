acfc3e6300f9480fee3655fd4decb531ab1edc70
r-svp26 <riteshk6341@gmail.com>
2021-09-10 16:42:58 +0530

    [Ritesh] Add: Calculate employee wage

--- Employee.js
+++ Employee.js
@@ -1,9 +1,25 @@
-/* UC1-employee attendence */
-const IS_PRESENT = 1;
-let empCheck = Math.floor(Math.random()* 10) % 2;
-if(empCheck == IS_PRESENT){
-    console.log("Employee is present");
-    return;
-} else{
-    console.log("Employee is absent");
+/* UC2-calculate employee wage */
+
+// constants 
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const WAGE_PER_HOUR = 20;
+// variable
+let empHrs, empWage;
+
+let empCheck = Math.floor(Math.random()* 10) % 3;
+switch (empCheck){
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break;
+    case IS_PART_TIME:
+        empHrs = 4;
+        break;
+    default:
+        empHrs = 0;
+        break;
 }
+empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage:" + empWage);
+
+
