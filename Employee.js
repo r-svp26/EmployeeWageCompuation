a4589aee8723bea4b11623258bab3c717487a6e1
r-svp26 <riteshk6341@gmail.com>
2021-09-10 16:57:51 +0530

    [Ritesh] Refactor: Get daily working hours using function

--- Employee.js
+++ Employee.js
@@ -1,5 +1,16 @@
 /* UC2-calculate employee wage */
 
+// function to get daily working hours
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_FULL_TIME:
+            return 8;
+        case IS_PART_TIME:
+            return 4;
+        default:
+            return 0;
+    } 
+}
 // constants 
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
@@ -8,17 +19,7 @@ const WAGE_PER_HOUR = 20;
 let empHrs, empWage;
 
 let empCheck = Math.floor(Math.random()* 10) % 3;
-switch (empCheck){
-    case IS_FULL_TIME:
-        empHrs = 8;
-        break;
-    case IS_PART_TIME:
-        empHrs = 4;
-        break;
-    default:
-        empHrs = 0;
-        break;
-}
+empHrs=getWorkingHours(empCheck);
 empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage:" + empWage);
 
